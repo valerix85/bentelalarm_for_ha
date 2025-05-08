b717f29de693e8567e812bb9f7f54e8f160e022c
valerix85 <93933385+techunleadit@users.noreply.github.com>
2025-05-08 22:14:04 +0200

    Update config_flow.py

--- custom_components/bentel_absoluta/config_flow.py
+++ custom_components/bentel_absoluta/config_flow.py
@@ -6,6 +6,7 @@ from .const import DOMAIN, DEFAULT_PORT
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class BentelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
@@ -19,6 +20,7 @@ class BentelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             try:
                 # Test di apertura sessione
                 from .utils import BentelProtocol
+
                 proto = BentelProtocol(host, port)
                 await proto.connect()
                 await proto.disconnect()
@@ -29,10 +31,12 @@ class BentelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     title=host,
                     data={"host": host, "port": port},
                 )
-        data_schema = vol.Schema({
-            vol.Required("host"): str,
-            vol.Optional("port", default=DEFAULT_PORT): int,
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required("host"): str,
+                vol.Optional("port", default=DEFAULT_PORT): int,
+            }
+        )
         return self.async_show_form(
             step_id="user",
             data_schema=data_schema,
